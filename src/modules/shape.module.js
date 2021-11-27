cc7abc39db8360785f1846b23109d145dab36164
Jackie-Ch <79127876+Jackie-Ch@users.noreply.github.com>
2021-11-27 16:07:13 +0500

    Implemented shape  module

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,26 @@
 import {Module} from '../core/module'
 
 export class ShapeModule extends Module {
+      // const divShape = document.createElement('div')
+      // divShape.className = 'shape'
+      // document.body.append(divShape)
+      // const shape = document.querySelector('.shape')
 
+      getRandomShape() {
+            if (Math.random() <= 0.2){
+            divShape.className = "square";
+            }
+            else if (Math.random() <= 0.4){
+            divShape.className = "circle";
+            }
+            else if (Math.random() <= 0.6){
+            divShape.className = "rectangle";
+            }
+            else if (Math.random() <= 0.8){
+            divShape.className = "trapezoid";
+            }
+            else {
+            divShape.className = "triangle";
+            }
+      }
 }
