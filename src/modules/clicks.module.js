320d6f0d904fb7713b89348dd15964888413656d
Aleksey <tolstuy.paren@gmail.com>
2021-11-28 19:37:45 +0200

    click module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -3,9 +3,27 @@ import {Module} from '../core/module'
 export class ClicksModule extends Module {
   constructor(type) {
     super(type, 'text');
+
+    this.oneClick = -1
+    this.twoClick = 0
   }
 
   trigger() {
+    let countClick = ()=>{
+      document.body.addEventListener('click',(event)=>{
+          event.preventDefault()
+          this.oneClick++
+          })
+  
+         document.addEventListener('contextmenu',(event)=>{
+         event.preventDefault()
+            this.twoClick++
+          })
+        setTimeout(() => { 
+          alert(`Кол-во нажатий левой кнопкой мыши: ${this.oneClick} , Кол-во нажатий правой кнопкой мыши: ${this.twoClick}`)
+       }, 3000); 
+  }
+  countClick()
     console.log('ClicksModule');
   }
 }
